967daa1edbb1541c956030c6645f994ea384ceeb
Mikhail Kolesnikov <89930451+mihailkol@users.noreply.github.com>
2025-03-12 22:59:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,25 +16,117 @@ class GsmDtmfAdapter extends utils.Adapter {
     async onReady() {
         this.log.info('Adapter is ready');
 
-        // Загрузка настроек
-        const modemPort = this.config.modemPort;
-        const baudRate = this.config.baudRate;
+        // Создаём папки и объекты
+        await this.createFoldersAndObjects();
 
         // Инициализация модема
-        this.modem = new SerialPort({
-            path: modemPort,
-            baudRate: baudRate,
-        });
-
-        // Используем ReadlineParser вместо Readline
-        const parser = this.modem.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-        parser.on('data', this.handleModemData.bind(this));
-
-        // Загрузка пользователей и устройств
-        this.users = this.config.users || [];
-        this.devices = this.config.devices || [];
+        await this.initializeModem();
 
         this.log.info('Adapter initialized');
+    }
+
+    async createFoldersAndObjects() {
+        // Создаём папку для пользователей
+        await this.setObjectNotExistsAsync('users', {
+            type: 'folder',
+            common: {
+                name: 'Users',
+                desc: 'Folder for storing users',
+            },
+            native: {},
+        });
+
+        // Создаём папку для устройств
+        await this.setObjectNotExistsAsync('devices', {
+            type: 'folder',
+            common: {
+                name: 'Devices',
+                desc: 'Folder for storing devices',
+            },
+            native: {},
+        });
+
+        // Создаём объект для настроек модема
+        await this.setObjectNotExistsAsync('modemSettings', {
+            type: 'state',
+            common: {
+                name: 'Modem Settings',
+                desc: 'Modem port and baud rate',
+                type: 'object',
+                role: 'settings',
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+
+        // Загружаем настройки модема
+        const modemSettings = await this.getStateAsync('modemSettings');
+        if (modemSettings && modemSettings.val) {
+            this.config.modemPort = modemSettings.val.port || '/dev/ttyUSB0';
+            this.config.baudRate = modemSettings.val.baudRate || 9600;
+        } else {
+            // Устанавливаем настройки по умолчанию
+            await this.setStateAsync('modemSettings', {
+                val: {
+                    port: '/dev/ttyUSB0',
+                    baudRate: 9600,
+                },
+                ack: true,
+            });
+        }
+    }
+
+    async initializeModem() {
+        // Инициализация модема
+        this.modem = new SerialPort({
+            path: this.config.modemPort,
+            baudRate: this.config.baudRate,
+        });
+
+        // Используем ReadlineParser для обработки данных
+        const parser = this.modem.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+        parser.on('data', this.handleModemData.bind(this));
+    }
+
+    async addUser(user) {
+        // Создаём объект для пользователя
+        const userId = `users.${user.name.replace(/\s/g, '_')}`;
+        await this.setObjectNotExistsAsync(userId, {
+            type: 'state',
+            common: {
+                name: user.name,
+                desc: `User ${user.name}`,
+                type: 'object',
+                role: 'info',
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+
+        // Сохраняем данные пользователя
+        await this.setStateAsync(userId, { val: user, ack: true });
+    }
+
+    async addDevice(device) {
+        // Создаём объект для устройства
+        const deviceId = `devices.${device.managedObject.replace(/\s/g, '_')}`;
+        await this.setObjectNotExistsAsync(deviceId, {
+            type: 'state',
+            common: {
+                name: device.managedObject,
+                desc: `Device ${device.managedObject}`,
+                type: 'object',
+                role: 'info',
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+
+        // Сохраняем данные устройства
+        await this.setStateAsync(deviceId, { val: device, ack: true });
     }
 
     onUnload(callback) {
@@ -71,13 +163,22 @@ class GsmDtmfAdapter extends utils.Adapter {
     }
 
     handleDtmfCommand(dtmf) {
-        const device = this.devices.find(d => d.dtmfCommand === dtmf);
-        if (device) {
-            this.log.info(`Device found: ${device.managedObject}`);
-            this.controlDevice(device);
-        } else {
-            this.log.warn(`No device found for DTMF command: ${dtmf}`);
-        }
+        // Поиск устройства по DTMF-команде
+        this.getDevices().then(devices => {
+            const device = devices.find(d => d.val.dtmfCommand === dtmf);
+            if (device) {
+                this.log.info(`Device found: ${device.val.managedObject}`);
+                this.controlDevice(device.val);
+            } else {
+                this.log.warn(`No device found for DTMF command: ${dtmf}`);
+            }
+        });
+    }
+
+    async getDevices() {
+        // Получаем список всех устройств
+        const devices = await this.getStatesAsync('devices.*');
+        return Object.values(devices);
     }
 
     controlDevice(device) {
